df8e4d073a0ca05489f2279e706185d5e4ec8524
jpmunich <parkercoyle8@gmail.com>
2022-05-29 09:58:35 -0400

    Added some basic UI as well as allowing the javascript function that checks for the palindrome run through console.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+userInput = document.getElementById("palindrome");
+button = document.getElementById("palindrome-button");
+
 // Function that takes an input and checks if the input is a palindrome
 function checkPalindrome(input) {
     // regex strips all non-alphanumeric characters
@@ -5,9 +8,17 @@ function checkPalindrome(input) {
 
     // Makes stripped string into an array, flips it, then joins it back to a string
     reverseInput = reverseInput.split('').reverse().join('');
-    
-    if (reverseInput == input) {
+
+    if (reverseInput === input) {
+        console.log("hello");
         return "Is a palindrome";
     }
+    console.log("bye");
     return "Not a palindrome";
 }
+// Function exists only to allow an argument to be passed through the event listener.
+function hello (){
+    checkPalindrome(userInput.value);
+}
+
+button.addEventListener("click", hello);
